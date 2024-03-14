560468858603a4b4d1d5295891397b4ebb8cdf1e
qjvk2880 <qjvk2880@gmail.com>
2024-03-15 00:56:14 +0900

    Refactor : 샘플코드 삭제

--- src/main/java/com/cotato/networking1/Networking1Application.java
+++ src/main/java/com/cotato/networking1/Networking1Application.java
@@ -10,13 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 @RequiredArgsConstructor
 public class Networking1Application {
-    private final SampleRepository sampleRepository;
     public static void main(String[] args) {
         SpringApplication.run(Networking1Application.class, args);
-    }
-    @PostConstruct
-    private void postConstruct() {
-        sampleRepository.save(new SampleEntity(1L,"sample1"));
-        sampleRepository.save(new SampleEntity(2L,"sample2"));
     }
 }
